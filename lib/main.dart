b97e0f6ac370aec899395f944cb369bccac209e0
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-03-14 19:34:27 +0200

    Basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:eventify_frontend/views/events_view.dart';
+import 'package:eventify_frontend/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'views/map_view.dart';
 
 /*void main() {
   runApp(MyApp());
@@ -14,6 +17,13 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  int _state = 0;
+
+  void _stateCounter(int i) {
+    setState(() {
+      _state = i;
+    });
+  }
   // code here
 
   @override
@@ -22,10 +32,52 @@ class MyAppState extends State<MyApp> {
 
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
-      body: Text('Default text!'),
-    ));
+            appBar: AppBar(
+              title: Text('My App'),
+            ),
+            body: WillPopScope(
+                // Takaisinnäppäimen hallinta, muista näkymistä vie homeen ja homesta sulkee sovelluksen
+                onWillPop: () async {
+                  if (_state != 0) {
+                    setState(() {
+                      _state = 0;
+                    });
+                    return false;
+                  }
+                  return true;
+                },
+                // Näkymät: state 0 = home, state 1 = events, state 2 = map
+                child: Column(
+                    children: ([
+                  _state == 0
+                      ? (Expanded(flex: 2, child: homeView))
+                      : _state == 1
+                          ? (Expanded(flex: 2, child: eventsView))
+                          : (Expanded(flex: 2, child: mapView)),
+
+                  // Painikkeet alhaalla: events, home, map
+                  Container(
+                      color: Colors.orange,
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(10.0),
+                      child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextButton(
+                                  onPressed: () => _stateCounter(0),
+                                  child: const Text('HOME'),
+                                ),
+                                TextButton(
+                                  onPressed: () => _stateCounter(1),
+                                  child: const Text('EVENTS'),
+                                ),
+                                TextButton(
+                                  onPressed: () => _stateCounter(2),
+                                  child: const Text('MAP'),
+                                )
+                              ])))
+                ])))));
   }
 }
---
+++ lib/views/events_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+@override
+Widget eventsView = Container(
+    width: double.infinity,
+    height: 100.0,
+    padding: const EdgeInsets.all(10.0),
+    child: Align(
+        alignment: Alignment.centerLeft, child: Text('This is events view')));
