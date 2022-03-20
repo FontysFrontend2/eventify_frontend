d44160c819570338af554188211a884a1171ed38
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-03-20 17:13:42 +0200

    version fixed

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class MapView extends StatelessWidget {
   const MapView({Key? key}) : super(key: key);
 
@@ -10,11 +12,27 @@ class MapView extends StatelessWidget {
         width: double.infinity,
         height: 100.0,
         padding: const EdgeInsets.all(10.0),
-        child: const Align(
-            alignment: Alignment.center,
-            child: Text(
-              'This is map view',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            )));
+        child: const Align(alignment: Alignment.center, child: MapScreen()));
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  static const _initialCameraPosition =
+      CameraPosition(target: LatLng(65.012615, 25.471453), zoom: 11.5);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GoogleMap(
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: false,
+            initialCameraPosition: _initialCameraPosition));
   }
 }
