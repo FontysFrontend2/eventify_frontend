7f3ba440bee05df9450e3e1adb943d1ed25c9dea
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-03-29 12:49:27 +0300

    create event

--- lib/create_event/create_event_view.dart
+++ lib/create_event/create_event_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CreateEventView extends StatelessWidget {
   const CreateEventView({Key? key}) : super(key: key);
 
+// create event form
   @override
   Widget build(BuildContext context) {
     return Container(
