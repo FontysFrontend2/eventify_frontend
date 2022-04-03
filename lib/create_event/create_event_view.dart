c2936e240dd4c23947418205c15969055f6c7e76
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-04-03 11:08:44 +0300

    cleaning up

--- lib/create_event/create_event_view.dart
+++ lib/create_event/create_event_view.dart
@@ -44,134 +44,168 @@ class _NewEventFormState extends State<NewEventForm> {
     super.dispose();
   }
 
+// SCREEN:
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Container(
-          width: double.infinity,
-          color: Colors.amber,
-          padding: const EdgeInsets.all(20),
-          alignment: Alignment.center,
-          margin: const EdgeInsets.only(bottom: 20),
-          child: const Text(
-            'Create new event',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          )),
+      viewTitle(context),
       Form(
           key: _formKey,
           child: Column(
             children: [
-              const Text('Event name:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  )),
-              Container(
-                  padding: EdgeInsets.all(20),
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    controller: nameController,
-                    validator: (eventValue) {
-                      if (eventValue == null || eventValue.isEmpty) {
-                        return 'The event needs a title';
-                      }
-                      return null;
-                    },
-                  )),
-              const Text('Event description:',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              Container(
-                  padding: EdgeInsets.all(20),
-                  child: TextFormField(
-                    controller: descriptionController,
-                    validator: (descriptionValue) {
-                      if (descriptionValue == null ||
-                          descriptionValue.isEmpty) {
-                        return 'The event needs a description';
-                      }
-
-                      return null;
-                    },
-                  )),
-              ElevatedButton(
-                  onPressed: () => _showTagSelection(context),
-                  child: const Text('Select tags')),
-              const Text('Selected tags:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  )),
-              Text(_type),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Use Location'),
-                  Switch(
-                      value: _useLocation,
-                      onChanged: (bool newValue) {
-                        setState(() {
-                          _useLocation = newValue;
-                        });
-                      }),
-                ],
-              ),
-              (_useLocation)
-                  ? (Container(
-                      color: Colors.amber,
-                      padding: const EdgeInsets.only(
-                          top: 10, bottom: 10, left: 40, right: 40),
-                      child: Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () => _showLocationSelection(context),
-                            child: const Text('Set Location',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                          Column(children: [
-                            const Text('Location for your event:',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Text(_posLat + ' ' + _posLong),
-                          ]),
-                        ],
-                      )))
-                  : (Container()),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    if (_useLocation && _posLat == '') {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text(
-                                'If you are using location You have to set it first!')),
-                      );
-                    } else {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      sendData(nameController.text, descriptionController.text,
-                          _posLat, _posLong);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content:
-                                Text('Processing Data: ' + infoTestString)),
-                      );
-                    }
-                  }
-                },
-                child: const Text('SUBMIT',
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
+              titleForm(context),
+              descriptionForm(context),
+              selectTagsForm(context),
+              selectLocationForm(context),
+              submit(context),
             ],
           ))
     ]);
   }
 
-// event location is in select location class under variable location as a string
+// WIDGETS:
+
+  Widget viewTitle(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        color: Colors.amber,
+        padding: const EdgeInsets.all(20),
+        alignment: Alignment.center,
+        //margin: const EdgeInsets.only(bottom: 20),
+        child: const Text(
+          'Create new event',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ));
+  }
+
+  Widget titleForm(BuildContext context) {
+    return Column(children: [
+      const Text('Event title:',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          )),
+      TextFormField(
+          textAlign: TextAlign.center,
+          controller: nameController,
+          validator: (eventValue) {
+            if (eventValue == null || eventValue.isEmpty) {
+              return 'The event needs a title';
+            }
+            return null;
+          }),
+    ]);
+  }
+
+  Widget descriptionForm(BuildContext context) {
+    return Column(children: [
+      const Text('Event description:',
+          style: TextStyle(fontWeight: FontWeight.bold)),
+      TextFormField(
+        textAlign: TextAlign.center,
+        controller: descriptionController,
+        validator: (descriptionValue) {
+          if (descriptionValue == null || descriptionValue.isEmpty) {
+            return 'The event needs a description';
+          }
+
+          return null;
+        },
+      )
+    ]);
+  }
+
+  Widget selectTagsForm(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+            onPressed: () => _showTagSelection(context),
+            child: const Text('Select tags')),
+        const Text('Selected tags:',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
+        Text(_type),
+      ],
+    );
+  }
+
+  Widget selectLocationForm(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Use Location'),
+            Switch(
+                value: _useLocation,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _useLocation = newValue;
+                  });
+                }),
+          ],
+        ),
+        (_useLocation)
+            ? (Container(
+                color: Colors.amber,
+                padding: const EdgeInsets.only(
+                    top: 10, bottom: 10, left: 40, right: 40),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _showLocationSelection(context),
+                      child: const Text('Set Location',
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                    Column(children: [
+                      const Text('Location for your event:',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(_posLat + ' ' + _posLong),
+                    ]),
+                  ],
+                )))
+            : (Container())
+      ],
+    );
+  }
+
+// submit button and handling for the information
+  Widget submit(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          if (_useLocation && (_posLat == '' || _posLat == 'null')) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                  content: Text(
+                      'If you are using location You have to set it first!')),
+            );
+          } else {
+            // If the form is valid, display a snackbar. In the real world,
+            // you'd often call a server or save the information in a database.
+            sendData(nameController.text, descriptionController.text, _posLat,
+                _posLong);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Processing Data: ' + infoTestString)),
+            );
+          }
+        }
+      },
+      child: const Text('SUBMIT',
+          style: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          )),
+    );
+  }
+
+  // sends data to database
   void sendData(
       String name, String description, String posLat, String posLong) {
     if (!_useLocation) {
@@ -193,7 +227,9 @@ class _NewEventFormState extends State<NewEventForm> {
     }
   }
 
-// event location is in select location class under variable location as a string
+  // LOCATION SET AND TAGS SET HANDLING:
+
+// gets location from location class
   void _showLocationSelection(BuildContext context) async {
     // Navigator.push returns a Future that completes after calling
     // Navigator.pop on the Selection Screen.
@@ -211,6 +247,7 @@ class _NewEventFormState extends State<NewEventForm> {
     });
   }
 
+// gets tags from tag class
   void _showTagSelection(BuildContext context) async {
     // Navigator.push returns a Future that completes after calling
     // Navigator.pop on the Selection Screen.
