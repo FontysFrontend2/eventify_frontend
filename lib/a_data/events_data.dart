fd7ac62d045ca9a864a6a90249550a516679370e
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-04-03 15:13:13 +0300

    events data on its on file

---
+++ lib/a_data/events_data.dart
@@ -0,0 +1,173 @@
+List eventsWithLocation = [
+  {
+    "id": 0,
+    "description": "Picnic at the park",
+    "interests": [0],
+    "members": [0],
+    "title": "Picnic",
+    "locationBased": true,
+    "latitude": 65.012615,
+    "longitude": 25.412615,
+    "hostID": 0,
+    "maxPeople": 10,
+    "minPeople": 0,
+    "startEvent": "2022-04-25T11:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 1,
+    "description": "Tournamnet for everyone interested",
+    "interests": [0],
+    "members": [0],
+    "title": "Football Tournament",
+    "locationBased": true,
+    "latitude": 65.022615,
+    "longitude": 25.481453,
+    "hostID": 1,
+    "maxPeople": 8,
+    "minPeople": 0,
+    "startEvent": "2022-04-14T08:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 2,
+    "description": "Cooking at the park with strangers",
+    "interests": [0],
+    "members": [0],
+    "title": "Cooking at the park",
+    "locationBased": true,
+    "latitude": 25.482453,
+    "longitude": 25.482453,
+    "hostID": 2,
+    "maxPeople": 0,
+    "minPeople": 24,
+    "startEvent": "2022-22-03T14:34:59.506Z",
+    "hasStarted": true
+  },
+  {
+    "id": 3,
+    "description": "Lets play airsoft for fun",
+    "interests": [0],
+    "members": [0],
+    "title": "Airsoft",
+    "locationBased": true,
+    "latitude": 65.012315,
+    "longitude": 25.492615,
+    "hostID": 0,
+    "maxPeople": 10,
+    "minPeople": 0,
+    "startEvent": "2022-04-25T11:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 4,
+    "description": "Swimming with people who has interested",
+    "interests": [0],
+    "members": [0],
+    "title": "Swimming",
+    "locationBased": true,
+    "latitude": 65.025615,
+    "longitude": 25.421453,
+    "hostID": 1,
+    "maxPeople": 8,
+    "minPeople": 0,
+    "startEvent": "2022-04-14T08:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 5,
+    "description": "Lets partyyy",
+    "interests": [0],
+    "members": [0],
+    "title": "Party at the beach",
+    "locationBased": true,
+    "latitude": 25.512453,
+    "longitude": 25.502453,
+    "hostID": 2,
+    "maxPeople": 0,
+    "minPeople": 24,
+    "startEvent": "2022-22-03T14:34:59.506Z",
+    "hasStarted": true
+  }
+];
+
+List eventsOfInterest = [
+  {
+    "id": 3,
+    "description": "Lets play airsoft for fun",
+    "interests": [0],
+    "members": [0],
+    "title": "Airsoft",
+    "locationBased": true,
+    "latitude": 65.012315,
+    "longitude": 25.492615,
+    "hostID": 0,
+    "maxPeople": 10,
+    "minPeople": 0,
+    "startEvent": "2022-04-25T11:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 4,
+    "description": "Swimming with people who has interested",
+    "interests": [0],
+    "members": [0],
+    "title": "Swimming",
+    "locationBased": true,
+    "latitude": 65.025615,
+    "longitude": 25.421453,
+    "hostID": 1,
+    "maxPeople": 8,
+    "minPeople": 0,
+    "startEvent": "2022-04-14T08:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 5,
+    "description": "Lets partyyy",
+    "interests": [0],
+    "members": [0],
+    "title": "Party at the beach",
+    "locationBased": true,
+    "latitude": 25.512453,
+    "longitude": 25.502453,
+    "hostID": 2,
+    "maxPeople": 0,
+    "minPeople": 24,
+    "startEvent": "2022-22-03T14:34:59.506Z",
+    "hasStarted": true
+  }
+];
+
+List eventsWithNoLocation = [
+  {
+    "id": 5,
+    "description": "CS Tournament for noobs and amateurs",
+    "interests": [0],
+    "members": [0],
+    "title": "CS Tournament",
+    "locationBased": false,
+    "latitude": 0,
+    "longitude": 0,
+    "hostID": 0,
+    "maxPeople": 10,
+    "minPeople": 0,
+    "startEvent": "2022-04-28T10:34:59.506Z",
+    "hasStarted": false
+  },
+  {
+    "id": 6,
+    "description": "Studying together, everyone welcome!",
+    "interests": [0],
+    "members": [0],
+    "title": "Study together",
+    "locationBased": false,
+    "latitude": 0,
+    "longitude": 0,
+    "hostID": 1,
+    "maxPeople": 8,
+    "minPeople": 0,
+    "startEvent": "2022-05-14T02:24:59.506Z",
+    "hasStarted": false
+  }
+];
--- lib/map/map_view.dart
+++ lib/map/map_view.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:eventify_frontend/event/eventcard_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../a_data/events_data.dart';
 
 class MapView extends StatefulWidget {
   // ignore: use_key_in_widget_constructors
@@ -97,53 +98,9 @@ class _MapScreenState extends State<MapView> {
     log(_filtered.toString());
 
 // Get list items from api
-    List markersFromApi = [
-      {
-        'id': '1',
-        'title': 'event 1',
-        'snippet': 'this is event 1',
-        'lat': 65.012615,
-        'long': 25.412615
-      },
-      {
-        'id': '2',
-        'title': 'event 2',
-        'snippet': 'this is event 2',
-        'lat': 65.022615,
-        'long': 25.481453
-      },
-      {
-        'id': '3',
-        'title': 'event 3',
-        'snippet': 'this is event 3',
-        'lat': 65.002616,
-        'long': 25.441483
-      },
-      {
-        'id': '4',
-        'title': 'event 4',
-        'snippet': 'this is event 4',
-        'lat': 65.012625,
-        'long': 25.482453
-      },
-    ];
+    List markersFromApi = eventsWithLocation;
 
-    List filteredMarkersFromApi = [
-      {
-        'id': '1',
-        'title': 'event 1',
-        'snippet': 'this is event 1',
-        'lat': 65.012615,
-        'long': 25.412615
-      },
-      {
-        'id': '3',
-        'title': 'event 3',
-        'snippet': 'this is event 3',
-        'lat': 65.002616,
-        'long': 25.441483
-      },
-    ];
+    List filteredMarkersFromApi = eventsOfInterest;
 
     setState(() {
       markerlist.clear();
@@ -158,17 +115,17 @@ class _MapScreenState extends State<MapView> {
       }
       log('markers' + setMarkers.toString());
       for (var element in setMarkers) {
-        String markervalue = setMarkers[counter]['id'];
+        String markervalue = setMarkers[counter]['id'].toString();
         log(element.toString());
         markerlist.add(Marker(
           //add first marker
-          markerId: MarkerId(setMarkers[counter]['id']),
-          position: LatLng(setMarkers[counter]['lat'],
-              setMarkers[counter]['long']), //position of marker
+          markerId: MarkerId(setMarkers[counter]['id'].toString()),
+          position: LatLng(setMarkers[counter]['latitude'.toString()],
+              setMarkers[counter]['longitude'.toString()]), //position of marker
           infoWindow: InfoWindow(
             //popup info
             title: setMarkers[counter]['title'],
-            snippet: setMarkers[counter]['snippet'] + ' tap to join',
+            snippet: setMarkers[counter]['description'] + ' tap to join',
             onTap: () => selectEvent(markervalue),
           ),
           icon: BitmapDescriptor.defaultMarker, //Icon for Marker
