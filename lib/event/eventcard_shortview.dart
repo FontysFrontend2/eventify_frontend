92c4880fec8a8582d47ac6813c84e434a7785de1
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-04-08 12:53:36 +0300

    EventShortCard

--- lib/event/eventcard_shortview.dart
+++ lib/event/eventcard_shortview.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
 class EventCardShortView extends StatelessWidget {
-  var e1 = EventData();
+  final d;
+
+  EventCardShortView(this.d);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightGreen,
-      width: double.infinity,
-      height: 100.0,
-      padding: const EdgeInsets.all(10.0),
-      child: Text(e1.name),
+    return Center(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(40),
+          onTap: () {},
+          child: SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: Column(
+              children: [
+                Text(d['name'].toString()),
+                Text(d['desc'].toString()),
+                Text(d['long'].toString()),
+                Text(d['lat'].toString()),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
-}
-
-class EventData {
-  String name = 'Tapahtuma1';
-  String desc = 'Testitapahtuma';
-  double long = 123.12;
-  double lat = 123.12;
 }
--- lib/event/eventcard_view.dart
+++ lib/event/eventcard_view.dart
@@ -7,15 +7,17 @@ class EventCardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.lightGreen,
-        width: double.infinity,
-        height: 100.0,
-        padding: const EdgeInsets.all(10.0),
-        child: Align(
-            alignment: Alignment.center,
-            child: Text(
-              'This is Eventcard view\n Event id is: ' + id!,
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            )));
+      color: Colors.lightGreen,
+      width: double.infinity,
+      height: 100.0,
+      padding: const EdgeInsets.all(10.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          'This is Eventcard view\n Event id is: ' + id!,
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
