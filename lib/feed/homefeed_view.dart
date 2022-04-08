514c25432d9e14840ed3475b28aa1e6808406204
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-04-08 13:37:42 +0300

    fix

--- lib/feed/homefeed_view.dart
+++ lib/feed/homefeed_view.dart
@@ -2,25 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:eventify_frontend/event/eventcard_shortview.dart';
 
 class HomeFeedView extends StatelessWidget {
-  HomeFeedView({Key? key}) : super(key: key);
-
-  final d1 = [
-    {
-      'name': 'Tapahtuma1',
-      'desc': 'Testitapahtuma',
-      'long': 123.12,
-      'lat': 123.12,
-    },
-    {
-      'name': 'Tapahtuma2',
-      'desc': 'Testitapahtuma',
-      'long': 132.12,
-      'lat': 111.12,
-    }
-  ];
+  const HomeFeedView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final d1 = [
+      {
+        'name': 'Tapahtuma1',
+        'desc': 'Testitapahtuma',
+        'long': 123.12,
+        'lat': 123.12,
+      },
+      {
+        'name': 'Tapahtuma2',
+        'desc': 'Testitapahtuma',
+        'long': 132.12,
+        'lat': 111.12,
+      }
+    ];
+
     return Container(
       color: Colors.white,
       width: double.infinity,
