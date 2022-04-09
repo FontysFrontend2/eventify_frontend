2d597dd9bc27c0c505a3c7d211ff8452b36c62d5
Niko Tuulaniemi <niko.tuulaniemi@gmail.com>
2022-04-09 15:58:24 +0300

    EventCardShortView almost ready

--- lib/event/eventcard_shortview.dart
+++ lib/event/eventcard_shortview.dart
@@ -1,3 +1,5 @@
+import 'package:eventify_frontend/chat/event_chat/event_location.dart';
+import 'package:eventify_frontend/event/eventcard_view.dart';
 import 'package:flutter/material.dart';
 
 class EventCardShortView extends StatelessWidget {
@@ -7,20 +9,53 @@ class EventCardShortView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool locationBased = d['locationBased'] as bool;
+    bool limitedPeople = d['maxPeople'] != 0;
     return Center(
       child: Card(
+        color: Colors.pinkAccent,
         child: InkWell(
           splashColor: Colors.blue.withAlpha(40),
-          onTap: () {},
+          onTap: () {
+            //tähän täytyy vielä keksiä järkevä tapa avata eventcardview...
+          },
           child: SizedBox(
             width: double.infinity,
-            height: 100,
+            height: 120,
             child: Column(
               children: [
-                Text(d['name'].toString()),
-                Text(d['desc'].toString()),
-                Text(d['long'].toString()),
-                Text(d['lat'].toString()),
+                //Event title
+                Text(
+                  d['title'].toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                //Event Description
+                Text(
+                  d['description'].toString(),
+                  style: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                //Event time
+                Text(
+                  d['startEvent'].split('T').toString(),
+                ),
+                //Event participant count and max participants
+                (limitedPeople)
+                    ? (Text(d['members'].length.toString() +
+                        '/' +
+                        d['maxPeople'].toString()))
+                    : (Container())
+                /*
+                //Small map to show location in small scale.
+                (locationBased)
+                    ? (SizedBox(
+                        height: 80,
+                        child: EventLocation(d['latitude'], d['longitude'])))
+                    : (Container()),*/
               ],
             ),
           ),
--- lib/feed/homefeed_view.dart
+++ lib/feed/homefeed_view.dart
@@ -1,39 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:eventify_frontend/event/eventcard_shortview.dart';
+import '../a_data/events_data.dart' as event_data;
 
-class HomeFeedView extends StatelessWidget {
+class HomeFeedView extends StatefulWidget {
   const HomeFeedView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final d1 = [
-      {
-        'name': 'Tapahtuma1',
-        'desc': 'Testitapahtuma',
-        'long': 123.12,
-        'lat': 123.12,
-      },
-      {
-        'name': 'Tapahtuma2',
-        'desc': 'Testitapahtuma',
-        'long': 132.12,
-        'lat': 111.12,
-      }
-    ];
+  HomeFeedState createState() => HomeFeedState();
+}
 
+class HomeFeedState extends State<HomeFeedView> {
+  int state = 1;
+  final events = event_data.eventsWithLocation;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       width: double.infinity,
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          ...d1.map((e) {
-            return EventCardShortView(e);
-          }).toList(),
-        ],
-      ),
+      padding: const EdgeInsets.all(5.0),
+      child: (state == 1)
+          ? ListView(
+              children: [
+                ...events.map((e) {
+                  return EventCardShortView(e);
+                }).toList(),
+              ],
+            )
+          : (Container()),
     );
   }
 }
+  
+
 
 // open eventcard view = Column(children: const [Expanded(flex: 2, child: EventCardView('id'))])
