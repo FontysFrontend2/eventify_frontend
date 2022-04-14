6019dd409af6b4b14ec2c35be2cb9e76f2053f91
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-04-14 14:36:03 +0300

    offline data fix

--- lib/models/all_events_model.dart
+++ lib/models/all_events_model.dart
@@ -63,22 +63,24 @@ Future<List<AllEventsData>> fetchAllEventsData() async {
   }
   final response = await http
       .get(Uri.parse('http://office.pepr.com:25252/Event/getAllEvents'));
-  if (response.statusCode == 200) {
+  if (response.statusCode == 200 && response.body != '[]') {
     prefs.setString("allEvents", json.encode(eventsOfInterest));
     List jsonResponse = json.decode(response.body);
-    print(response.statusCode);
+    print('Request succeed with code 200: USING DATA FROM DATABASE');
     return jsonResponse
         .map((data) => new AllEventsData.fromJson(data))
         .toList();
   } else {
     late List jsonResponseOffline;
     if (prefsNoData) {
-      print('nodata');
+      print(
+          'Request failed and no data in Shared preferences: USING LOCAL DATA SAMPLES');
       jsonResponseOffline = eventsOfInterest;
     } else {
+      print(
+          'Request failed but there is recent data in Shared preferences: USING SHARED PREFERENCES');
       jsonResponseOffline = json.decode(prefs.getString("allEvents")!);
     }
-    prefs.setString("allEvents", json.encode(jsonResponseOffline));
     return jsonResponseOffline
         .map((data) => new AllEventsData.fromJson(data))
         .toList();
