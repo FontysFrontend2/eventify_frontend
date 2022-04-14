7fc0c8925099f5173b4d895788e218cd358cfd22
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-04-14 11:28:28 +0300

    update

--- lib/a_data/event_from_id.dart
+++ lib/a_data/event_from_id.dart
@@ -1,16 +1,16 @@
 const eventFromId = {
-  "id": 1,
+  "id": 0,
   "description":
-      "Swimming with people who has interested, Lets meet at the beach and then we go swimming and sauna is warming up of course. Wlcome!",
+      "CAN'T CONNECT TO SERVER, This is description when connection is offline, The event id is same for all the events when offline. So this is just test view for development",
   "interests": [0],
   "members": ['0', '1', '2', '3', '4', '5'],
-  "title": "Swimming",
+  "title": "CAN'T CONNECT TO SERVER -Title",
   "locationBased": true,
   "latitude": 65.025615,
   "longitude": 25.421453,
   "hostID": 1,
   "maxPeople": 8,
   "minPeople": 0,
-  "startEvent": "2022-04-14T08:34:59.506Z",
+  "startEvent": "2022-06-14T08:34:59.506Z",
   "hasStarted": false
 };
