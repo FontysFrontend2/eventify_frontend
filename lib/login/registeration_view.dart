b038adcc55d94213be0758789344e38bb620bf3d
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2022-05-07 18:32:16 +0300

    Login+register tehty

--- lib/login/registeration_view.dart
+++ lib/login/registeration_view.dart
@@ -1,20 +1,75 @@
+import 'package:eventify_frontend/apis/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 
-class RegisterationView extends StatelessWidget {
+class RegisterationView extends StatefulWidget {
   const RegisterationView({Key? key}) : super(key: key);
 
   @override
+  State<RegisterationView> createState() => _RegisterationViewState();
+}
+
+class _RegisterationViewState extends State<RegisterationView> {
+  final usernameCtrl = TextEditingController();
+
+  final emailCtrl = TextEditingController();
+
+  final passwdCtrl = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Eventify'),
+      ),
+      body: Container(
         color: Colors.grey,
         width: double.infinity,
-        height: 100.0,
         padding: const EdgeInsets.all(10.0),
-        child: const Align(
-            alignment: Alignment.center,
-            child: Text(
-              'This is registeration event view',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            )));
+        child: Column(children: [
+          const Text(
+            'Username:',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextFormField(
+            autofocus: false,
+            textAlign: TextAlign.center,
+            controller: usernameCtrl,
+            autocorrect: false,
+          ),
+          const Text(
+            'Email:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          TextFormField(
+            autofocus: false,
+            textAlign: TextAlign.center,
+            controller: emailCtrl,
+            autocorrect: false,
+          ),
+          const Text(
+            'Password:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          TextFormField(
+            autofocus: false,
+            textAlign: TextAlign.center,
+            controller: passwdCtrl,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              registerUser(usernameCtrl.text, emailCtrl.text, passwdCtrl.text);
+            },
+            child: const Text('Submit'),
+          ),
+        ]),
+      ),
+    );
   }
 }
