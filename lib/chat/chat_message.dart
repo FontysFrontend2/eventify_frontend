7523df84f2c318b886a9f049fa3f390ae47104fe
NituX <78910113+NituX@users.noreply.github.com>
2022-05-10 17:45:31 +0300

    chat done and working

--- lib/chat/chat_message.dart
+++ lib/chat/chat_message.dart
@@ -1,5 +1,20 @@
 class ChatMessage {
-  String messageContent;
-  String messageType;
-  ChatMessage({required this.messageContent, required this.messageType});
+  late int id;
+  late String message;
+  late String user;
+  late String chatRoom;
+  late String timestamp;
+
+  ChatMessage(this.id, this.message, this.user, this.chatRoom, this.timestamp);
+
+  ChatMessage.short(this.user, this.message);
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) {
+    final id = json['id'];
+    final message = json['message'];
+    final user = json['user'];
+    final chatRoom = json['chatRoom'];
+    final timestamp = json['timestamp'];
+    return ChatMessage(id, message, user, chatRoom, timestamp);
+  }
 }
